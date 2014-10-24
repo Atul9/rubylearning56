c7c5832c3af19e61939b5b62c3c44e246180a0bb
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-25 00:58:14 +0530

    Readability for deaf granny program
    We take care of Granny answering silence, but also take care of how the
    running code reads.  We create methods to help express the ideas in the
    running code, and when you read it, you understand the idea of the
    programs steps.  If you want to see implementation, you can read the
    methods, though we also pay attention to how the methods read as well.
    
    We use rubocop for style, and disable some cops when appropriate.

--- 3_week/6e_deaf_granny_part_1.rb
+++ 3_week/6e_deaf_granny_part_1.rb
@@ -1,11 +1,14 @@
+# rubocop:disable Style/BlockComments
 =begin
 Exercise6:
 Write a Deaf Grandma program.
-Whatever you say to grandma (whatever you type in), she should respond with HUH?! SPEAK UP, SONNY!,
-unless you shout it (type in all capitals).
-If you shout, she can hear you (or at least she thinks so) and yells back, NO, NOT SINCE 1938!
-To make your program really believable, have grandma shout a different year each time;
-maybe any year at random between 1930 and 1950. You can't stop talking to grandma until you shout BYE.
+Whatever you say to grandma (whatever you type in), she should respond with
+HUH?! SPEAK UP, SONNY!, unless you shout it (type in all capitals).
+If you shout, she can hear you (or at least she thinks so) and yells back, NO,
+NOT SINCE 1938!
+To make your program really believable, have grandma shout a different year
+each time; maybe any year at random between 1930 and 1950. You can't stop
+talking to grandma until you shout BYE.
 Adapted from Chris Pine's Book.
 For example:
 You enter: Hello Grandma
@@ -13,30 +16,35 @@ Grandma responds: HUH?! SPEAK UP, SONNY!
 You enter: HELLO GRANDMA
 Grandma responds: NO, NOT SINCE 1938!
 =end
-puts  'HEY THERE, SONNY! GRANDMA HAS GOT A HEARING PROBLEM SO PLEASE SHOUT'
-while true
-  said = gets.chomp
-  if said == 'BYE'
-    puts 'BYE SWEETIE!'
-    break
-  end
-  if said != said.upcase
-    puts 'HUH?! SPEAK UP, SONNY!'
-  else
-    random_year = 1930 + rand(21)
-    puts 'NO, NOT SINCE ' + random_year.to_s + '!'
-  end
+# doctest: yelled will return true if it is all uppercased
+# >> yelled? 'HELLO'
+# => true
+# doctest: yelled will return false if it is not all upcased
+# >> yelled? 'Not yelled'
+# => false
+# >> !! yelled?('')
+# => false
+def yelled?(words)
+  words == words.upcase unless words.empty?
 end
-=begin
-#Just to check how bye would work
-def bye?(get)
-  if get.chomp == "BYE"
-  puts 'bye'
-  exit
-  else
-    puts 'Say something...'
-  end
 
+def heard
+  remembered_year = 1930 + rand(21)
+  format('NO, NOT SINCE %i!', remembered_year)
 end
-bye?(gets)
-=end
+
+def unheard
+  'HUH?! SPEAK UP, SONNY!'
+end
+
+if __FILE__ == $PROGRAM_NAME # library guard or application guard
+  puts 'HEY THERE, SONNY! GRANDMA HAS GOT A HEARING PROBLEM SO PLEASE SHOUT'
+  until 'BYE' == said = gets.chomp # rubocop:disable Lint/AssignmentInCondition
+    if yelled? said
+      puts heard
+    else
+      puts unheard
+    end
+  end
+  puts 'BYE SWEETIE!'
+end
