ce61628cd4fff9dc9559516ab0d61d819df1f8b6
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-24 14:31:35 +0530

    Sum of elements

--- 3_week/10e_sum_of_elements.rb
+++ 3_week/10e_sum_of_elements.rb
@@ -2,8 +2,10 @@
 Exercise10. Write a Ruby program that, when given an array: collection = [1, 2, 3, 4, 5] calculates the sum of its elements.
 =end
 collection = [1, 2, 3, 4, 5]
+=begin
 puts 'Elements of collection: ',collection
 puts 'Sum of the elements : ',collection.reduce(:+)
+=end
 =begin
 Other solution
 collection = [1, 2, 3, 4, 5]
@@ -19,8 +21,15 @@ Another:
 def calculate_sum(array)
 array.reduce(:+)
 end
-
 if __FILE = $PROGRAM_NAME
 puts calculate_sum([1, 2, 3, 4, 5])
 end
 =end
+def calculate(collection)
+  sum = 0
+  for i in 0..4
+    sum = sum + collection[i]
+  end
+  puts 'Sum of the elements : ',sum
+end
+calculate(collection)
