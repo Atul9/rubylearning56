37d2ed9e3c39def7774f2923e990af53e5cc02d9
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-24 18:58:23 +0530

    key=value

--- 3_week/5e_key_=_value.rb
+++ 3_week/5e_key_=_value.rb
@@ -5,4 +5,7 @@ Hint: Use some of the String functions.
 s = 'key=value'
 s1, _, s2 = s.scan(/\w+|=/)
 puts 's: ',s,'s1: ',s1,'s2: ',s2
-
+# _  is legitimate var name, but usually used for throwaway values
+# Another solution:
+# s1, s2 = s.split('=')
+# => ["key","value"]
