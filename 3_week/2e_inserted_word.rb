ac20960b88a976e96746ffbdcc4eefd731aee24d
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-25 12:23:03 +0530

    Inserted word

--- 3_week/2e_inserted_word.rb
+++ 3_week/2e_inserted_word.rb
@@ -5,21 +5,28 @@ def replace_word(text,original_word, replaced_word)
   regex = %r[\b#{original_word}\b]
   text.gsub(regex,replaced_word)
 end
-filename = '/home/atul/RubyLearning/CoreBatch/56/3_week/3wk_2e_text.txt'
-puts 'Contents of the file: '
-File.open(filename,'r') do|file|
-  while line = file.gets
-    puts line
+# doctest: exploring ARGV and ARGF
+# >> ARGF
+# => ''
+if __FILE__ == $0
+  puts 'Enter the filename you want to edit'
+  filename = ARGF.to_s || gets.chomp
+  filename = '/home/atul/RubyLearning/CoreBatch/56/3_week/3wk_2e_text.txt'
+  puts 'Contents of the file: '
+  File.open(filename,'r') do|file|
+    while line = file.gets
+      puts line
+    end
   end
-end
-File.open(filename, 'r+') do |file|
-  text = file.readlines.join
-  file.seek(0)
-  file.write(replace_word(text,'word','inserted word'))
-end
-puts "\nContents of the file after making changes: "
-File.open(filename,'r') do|file|
-  while line = file.gets
-    puts line
+  File.open(filename, 'r+') do |file|
+    text = file.readlines.join
+    file.seek(0)
+    file.write(replace_word(text,'word','inserted word'))
+  end
+  puts "\nContents of the file after making changes: "
+  File.open(filename,'r') do|file|
+    while line = file.gets
+      puts line
+    end
   end
 end
