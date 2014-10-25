ed6c9f91506dc7bc42a8dc68956f46b2cbbf14ec
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-26 01:04:39 +0530

    Moved odd_or_even to Integer class
    We are not using refinements for this, but that is a good research
    subject.  We did monkey patch Integer directly.  Now we can use
    `odd_or_even` directly on the integer.

--- 3_week/11e_odd_or_even?.rb
+++ 3_week/11e_odd_or_even?.rb
@@ -3,22 +3,29 @@ Exercise11. Write a Ruby program that, when given an array: collection = [12,
 23, 456, 123, 4579] prints each number, and tells you whether it is odd or
 even.
 =end
-# doctest: odd_or_even returns "even" for even numbers
-# >> odd_or_even 12
-# => 'even'
-# doctest: odd_or_even returns 'odd' for odd numbers
-# >> odd_or_even 3
-# => 'odd'
-def odd_or_even(number)
-  # currently, this method does not do what the method name suggests
-  # It returns an array
-    number.even? ? 'even' : 'odd'
+class Integer
+  # doctest: odd_or_even returns "even" for even numbers
+  # >> 12.odd_or_even
+  # => 'even'
+  # doctest: odd_or_even returns 'odd' for odd numbers
+  # >> 3.odd_or_even
+  # => 'odd'
+  def odd_or_even
+    # currently, this method does not do what the method name suggests
+    # It returns an array
+    even? ? 'even' : 'odd'
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  collection = [12, 23, 456, 123, 4579]
+  collection = [12, 23, 456, 123, 4579, ]
   collection.each do |number|
-  puts "#{number} is #{odd_or_even(number)}."
+    puts "#{number} is #{number.odd_or_even}."
+  end
+
+  15.times do
+    number = rand(1.0e6)
+    puts "#{number} is #{number.odd_or_even}."
   end
 end
 # even? is a method of Integer. It returns true if int is even
