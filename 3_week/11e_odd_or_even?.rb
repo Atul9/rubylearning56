108cdb4130379f258f48f0802fe61dc28ee2f0ef
Victor Goff <keeperotphones@gmail.com>
2014-10-24 22:13:34 -0400

    Tests written for the odd_or_even method
    The odd_or_even method is kind of surprising for what it returns

--- 3_week/11e_odd_or_even?.rb
+++ 3_week/11e_odd_or_even?.rb
@@ -1,11 +1,22 @@
 =begin
 Exercise11. Write a Ruby program that, when given an array: collection = [12, 23, 456, 123, 4579] prints each number, and tells you whether it is odd or even.
 =end
-collection = [12, 23, 456, 123, 4579]
+# doctest: odd_or_even returns "even" for even numbers
+# >> odd_or_even 12
+# => true
+# doctest: odd_or_even returns 'odd' for odd numbers
+# >> odd_or_even 3
+# => false
 def odd_or_even(numbers)
+  # currently, this method does not do what the method name suggests
+  # It returns an array
   numbers.each do|n|
-  n.even? ? puts("#{n} is even") : puts("#{n} is odd")
+    n.even? ? puts("#{n} is even") : puts("#{n} is odd")
   end
 end
-odd_or_even(collection)
-# even is a method of Integer. It returns true if int is even
+
+if __FILE__ == $PROGRAM_NAME
+  collection = [12, 23, 456, 123, 4579]
+  odd_or_even(collection)
+end
+# even? is a method of Integer. It returns true if int is even
