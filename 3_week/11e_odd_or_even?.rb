0aaf365ee74600ff5724052ecae275660cb988f5
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-26 00:49:11 +0530

    Accept a number and give an answer for Even or Odd
    This is now simply doing the one thing with any given number.

--- 3_week/11e_odd_or_even?.rb
+++ 3_week/11e_odd_or_even?.rb
@@ -1,22 +1,24 @@
 =begin
-Exercise11. Write a Ruby program that, when given an array: collection = [12, 23, 456, 123, 4579] prints each number, and tells you whether it is odd or even.
+Exercise11. Write a Ruby program that, when given an array: collection = [12,
+23, 456, 123, 4579] prints each number, and tells you whether it is odd or
+even.
 =end
 # doctest: odd_or_even returns "even" for even numbers
 # >> odd_or_even 12
-# => true
+# => 'even'
 # doctest: odd_or_even returns 'odd' for odd numbers
 # >> odd_or_even 3
-# => false
-def odd_or_even(numbers)
+# => 'odd'
+def odd_or_even(number)
   # currently, this method does not do what the method name suggests
   # It returns an array
-  numbers.each do|n|
-    n.even? ? puts("#{n} is even") : puts("#{n} is odd")
-  end
+    number.even? ? 'even' : 'odd'
 end
 
 if __FILE__ == $PROGRAM_NAME
   collection = [12, 23, 456, 123, 4579]
-  odd_or_even(collection)
+  collection.each do |number|
+  puts "#{number} is #{odd_or_even(number)}."
+  end
 end
 # even? is a method of Integer. It returns true if int is even
