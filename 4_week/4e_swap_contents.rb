bb6601900963f2fa04f73b2d252c973ecdc92e8e
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-31 01:56:05 +0530

    Swap the contents

--- 4_week/4e_swap_contents.rb
+++ 4_week/4e_swap_contents.rb
@@ -4,7 +4,7 @@ following. Take two text files say A and B. The program should swap the
 contents of A and B. That is after the program is executed, A should contain
 B's contents and B should contains A's contents.
 =end
-
+=begin
 def filereadwrite(file1, file2)
   f1 = File.open(file1,"r")
   f2 = File.open(file2, "w")
@@ -17,3 +17,11 @@ end
 filereadwrite("A.txt","temp.txt")
 filereadwrite("B.txt","A.txt")
 filereadwrite("temp.txt","B.txt")
+=end
+
+def filereadwrite(file1,file2)
+  system 'mv', file1, "temp1.txt"
+  system 'mv', file2, file1
+  system 'mv', "temp1.txt", file2
+end
+filereadwrite('a1.txt','b1.txt')
