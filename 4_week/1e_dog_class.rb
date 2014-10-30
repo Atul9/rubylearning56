71785ebb388d75577ec4243707250243a2434597
Atul Bhosale <atul1bhosale@gmail.com>
2014-10-30 14:16:52 +0530

    Dog class

--- 4_week/1e_dog_class.rb
+++ 4_week/1e_dog_class.rb
@@ -1,22 +1,30 @@
 =begin
-Exercise 1
+Exercise1. Write a class called Dog, that has name as an instance variable and the following methods:
+
+bark(), eat(), chase_cat()
+I shall create the Dog object as follows:
+d = Dog.new('Leo')
 =end
 class Dog
 
   def initialize(name)
     @name = name
   end
+  def myname
+    @name
+  end
   def bark
-    'barks'
+    'Ruff! Ruff!'
   end
   def eats
-    'eats bones'
+    'eat bones'
   end
   def chase_cat
-    'chases Tom'
+    'Tom'
   end
 end
 d = Dog.new('Leo')
-puts "#{d} #{d.bark}"
-puts d,' ', d.eats
-puts d,' ',d.chase_cat
+puts "My name is #{d.myname}.\n #{d.bark}"
+puts "I eat #{d.eats}"
+puts "I chase #{d.chase_cat}"
+puts "My id is #{d.object_id}"
