86d3fd86abfc82b599b37e710eed4d9d8c05442a
Atul Bhosale <atul1bhosale@gmail.com>
2014-11-01 18:04:13 +0530

    sprintf

---
+++ 1_week/2e_sprintf.rb
@@ -0,0 +1,6 @@
+=begin
+Read the sprintf documentation and the % documentation in the String class and figure out the output being printed by of this Ruby code.
+puts "%05d" % 123
+=end
+puts "%05d" % 123
+# Returns a decimal number with a minimum width of 5 with padding 0's on the left
