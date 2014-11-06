21dd62cd7927e5e1166c9bd8255136806ff73bbc
KOTP <keeperotphones@gmail.com>
2014-11-06 19:18:17 +0000

    Directory inentory

--- 4_week/5e_dir_inventory.rb
+++ 4_week/5e_dir_inventory.rb
@@ -17,8 +17,14 @@ created.
 def display
   puts Dir.glob('**/*').sort
 end
-
+#display
 def find_new_files(old,new)
   IO.readlines(old) - IO.readlines(new)
 end
-puts find_new_files('old-inventory.txt','new-inventory.txt')
+#puts find_new_files('old-inventory.txt','new-inventory.txt')
+puts (IO.readlines('old-inventory.txt') - IO.readlines('new-inventory.txt')) # Used readlines since readline is a private method of the class IO. readlines is public.
+arr1 = IO.readlines('old-inventory.txt')
+arr2 = IO.readlines('new-inventory.txt')
+
+puts arr2 - arr1
+#puts [1,2,3] - [2,3,4] # We expect to see 1, as 2 and 3 are removed and 4 doesn't exist to remove from the array. Oh yes I got my mistake.
