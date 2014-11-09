579c71e9cf27e7f09cec259a3059a3eb0365be2d
KOTP <keeperotphones@gmail.com>
2014-11-09 15:40:58 +0000

    Age now starts with an actual age
    This removes the need to give the number of seconds for each method call
    on Age class.
    
    `.to_s` gives us the opinion of what we think should be repesented as a
    string.
    
    year_months gives the ability for the user to format their strings as
    they see fit, adding flexibility and freedom.
    
    Add some tests.  May still need to add more, but this should cover most
    bugs.
    
    Lesson learned:  An array can be used to return multiple values back to
    the user, because Ruby will only return one value, not multiples.
    
    Also, splat operator `*` will flatten an array within an array.

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -9,27 +9,49 @@
     1025600095
 =end
 class Age
-  def initialize
+  attr_reader :seconds
+  def initialize(seconds)
+    @seconds = seconds
     @seconds_in_a_month = 60 * 60 * 24 * 30
     @seconds_in_standard_year = 60 * 60 * 24 * 365
   end
 
-  def age_in_years(seconds)
-    seconds / @seconds_in_standard_year
+  # doctest: age in one year
+  # >> age = Age.new(60 * 60 * 24 * 365)
+  # >> age.age_in_years
+  # => 1
+  # doctest: age in 9.79e6 seconds for years
+  # >> Age.new(979000000).age_in_years
+  # => 31
+  def age_in_years
+    (seconds.to_f / @seconds_in_standard_year).floor
   end
 
-  def month_remainder(seconds)
+  def month_remainder
     (seconds % @seconds_in_standard_year) / @seconds_in_a_month
   end
 
-  def display_age(seconds)
-    puts "Age in seconds = #{seconds}. #{age_in_years(seconds)} years and #{month_remainder(seconds)} months old"
+  # doctest: should return only a year and a month
+  # >> '%i years and %i months old.' % Age.new(979000000).year_months
+  # => "31 years and 0 months old."
+  # doctest: for 9.79e4
+  # >> '%i years and %i months old.' % Age.new(9790000).year_months
+  # => "0 years and 3 months old."
+  def year_months
+    [age_in_years, month_remainder]
+  end
+
+  def to_s
+    '%i years and %i months' % year_months
   end
 end
 
-seconds = [979000000, 2158493738, 246144023, 1270166272, 1025600095]
-age = Age.new
+if __FILE__ == $0
+  seconds = [979000000, 2158493738, 246144023, 1270166272, 1025600095]
 
-seconds.each do |convert|
-  age.display_age(convert)
+  seconds.each do |s|
+    puts "For %s is %i years and %i months." %  [s, *Age.new(s).year_months]
+    # using the splat operator * in this way flattens the array contents, it
+    # causes [1, [1, 2]] to become [1, 1, 2]
+  end
 end
