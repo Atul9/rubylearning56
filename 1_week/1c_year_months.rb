1c84e53ae1ba2d0ae74b9c3eab9e56f2c2509658
Atul Bhosale <atul1bhosale@gmail.com>
2014-11-09 20:20:47 +0530

    Year and months challenge

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -1,17 +1,17 @@
 =begin
-Using what we have learned so far: 
-I wonder if anyone could have a crack at working out the age in years and months form.
-For example “I’m 32 years and 6 months old.” Use the following values for age_in_seconds:
-979000000
-2158493738
-246144023
-1270166272
-1025600095
+    Using what we have learned so far: 
+      I wonder if anyone could have a crack at working out the age in years and months form.
+      For example “I’m 32 years and 6 months old.” Use the following values for age_in_seconds:
+      979000000
+    2158493738
+    246144023
+    1270166272
+    1025600095
 =end
-class Age_challenge
+class Age
   def initialize
-    @seconds_in_a_month = 60* 60* 24*30
-    @seconds_in_standard_year = 60* 60* 24*365
+    @seconds_in_a_month = 60 * 60 * 24 * 30
+    @seconds_in_standard_year = 60 * 60 * 24 * 365
   end
 
   def age_in_years(seconds)
@@ -22,14 +22,14 @@ class Age_challenge
     (seconds % @seconds_in_standard_year) / @seconds_in_a_month
   end
 
-  def age_in_years_and_months(seconds)
+  def display_age(seconds)
     puts "Age in seconds = #{seconds}. #{age_in_years(seconds)} years and #{month_remainder(seconds)} months old"
   end
 end
 
 seconds = [979000000, 2158493738, 246144023, 1270166272, 1025600095]
-age = Age_challenge.new
+age = Age.new
 
 seconds.each do |convert|
-  age.age_in_years_and_months(convert)
+  age.display_age(convert)
 end
