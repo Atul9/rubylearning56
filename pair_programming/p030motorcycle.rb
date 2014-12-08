2ff35ca6f137a8f5adafaad5a4cb635beb638fb2
Atul9 <atul1bhosale@gmail.com>
2014-12-08 08:38:56 +0530

    Doubt about initializing an object

---
+++ pair_programming/p030motorcycle.rb
@@ -0,0 +1,22 @@
+# p030motorcycle.rb
+class MotorCycle
+  def initialize(make, color)
+    # Instance variables
+    @make = make
+    @color = color
+    end
+  def startEngine
+    if (@engineState)
+      puts 'Engine Running'
+    else
+      @engineState = true
+      puts 'Engine Idle'
+    end
+  end
+  def display
+    @make + " " + @color
+  end
+end
+m = MotorCycle.new(YAMAHA, black)
+puts m.display
+puts m.startEngine
