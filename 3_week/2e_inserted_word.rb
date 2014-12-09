bbdcfcea3cb068f7d001dc028f8209b86109a6e1
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2014-12-09 18:28:53 +0000

    Inserted word

--- 3_week/2e_inserted_word.rb
+++ 3_week/2e_inserted_word.rb
@@ -12,14 +12,10 @@ if __FILE__ == $0
   puts 'Enter the filename you want to edit'
   filename = ARGV[0] || '3wk_2e_text.txt'
   puts 'Contents of the file: '
-  File.open(filename,'r') do|file|
-    while line = file.gets # This reads line per line
-      puts line
-    end
-  end
+  puts File.open(filename, 'r').read
   File.open(filename, 'r+') do |file|
-    text = file.readlines.join # This creates an Array, and then rejoins the array to make a string.  Why not just use 'read'?
-    file.seek(0) # This seeks to the start of the file, but where else would we be if we are just opening a file
+    text = file.readlines.join 
+    file.seek(0)
 
     file.write(replace_word(text,'word','inserted word'))
   end
