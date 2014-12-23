83aee80e2e877cea6900af300756f1c7549b4f96
Atul9 <atul1bhosale@gmail.com>
2014-12-23 09:10:11 +0530

    Spec change exercise

---
+++ 5_week/2e_spec_change.rb
@@ -0,0 +1,43 @@
+=begin
+The Really Annoying Project Manager (Yes Boss) has made a spec change!
+
+"There will be an amoeba shape on the screen, with the others. When the user
+clicks on the amoeba, it will rotate like the others, and play a .hif sound
+file. The other figures like rectangle rotate around the center. I want the
+amoeba shape to rotate around a point on one end, like a clock hand !"
+=end
+class Shape
+  def initialize(shape = nil, sound = nil, direction = 'by 360 defrees', extension = 'aif')
+    @shape = self.class.name
+    @sound = self.class.name
+    @direction = direction
+    @extension = extension
+  end
+  
+  def rotate
+    "#{@shape} rotates #{@direction}"
+  end
+
+  def play
+    "Playing #{@sound}.#{@extension}"
+  end
+
+  def click
+    [rotate, play].join("\n")
+  end
+
+end
+
+class Square < Shape; end
+class Circle < Shape; end
+class Triangle < Shape; end
+class Amoeba < Shape; end
+
+[Square.new("square", "Guitar"),
+Triangle.new("Triangle", "Drums"),
+Circle.new,
+Amoeba.new("amoeba", "Nom Nom Nom", "around a point on one end", "hif")
+].each do |shape|
+  puts shape.click
+  puts
+end
