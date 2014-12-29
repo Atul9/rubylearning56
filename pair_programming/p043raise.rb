c5f7ce6e5847fc866b82b00f70637d6344f16df7
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2014-12-29 18:53:32 +0000

    Learning Exception Handling
    Learnt about raising an exceptio with raise. rescue is used to handle an
    exception.

--- pair_programming/p043raise.rb
+++ pair_programming/p043raise.rb
@@ -1,7 +1,11 @@
 # p043raise.rb
-def raise_exception
-  puts 'I am before the raise.'
-  raise 'An error has occured'
-  puts 'I am after the raise'
+def raise_exception(message='correct_input')
+    puts 'I am before the raise.'
+    fail 'An error has occured' if message == 'wrong_input'
+    return puts 'I am after the raise'
+  rescue RuntimeError => e
+    puts "Nothing is ran after raise, but I can rescue it... your error message is \"#{e.message}\""
 end
 raise_exception
+raise_exception 'wrong_input'
+#Once an exception is raised. The statements following dont get executed right?
--- pair_programming/p045handexcp.rb
+++ pair_programming/p045handexcp.rb
@@ -1,12 +1,45 @@
 # p045handexcp.rb
-def raise_and_rescue
+def default_raise_and_rescue
   begin 
     puts 'I am before the raise.'
-    raise 'An error has occured.'
+    raise SyntaxError, 'An error has occured.'
     puts 'I am after the raise.'
-  rescue
-    puts 'I am rescued.'
+  rescue RuntimeError => e
+    puts "We rescued your #{e.class}"
+    e.class # => 
   end
   puts 'I am after the begin block.'
 end
-raise_and_rescue
+# default_raise_and_rescue # => 
+
+class AtulError < Exception
+  def message
+    "You need to consult with Atul!"
+  end
+end
+def custom_raise_and_rescue
+  puts "I am before the raise."
+  raise AtulError
+  puts "I am after the AtulError."
+rescue => e
+  puts e.backtrace.inspect # => 
+end
+custom_raise_and_rescue
+
+
+
+
+
+
+
+
+
+
+
+# >> I am before the raise.
+
+# ~> AtulError
+# ~> You need to consult with Atul!
+# ~>
+# ~> /tmp/seeing_is_believing_temp_dir20141223-6649-15f1rm5/program.rb:22:in `custom_raise_and_rescue'
+# ~> /tmp/seeing_is_believing_temp_dir20141223-6649-15f1rm5/program.rb:27:in `<main>'
