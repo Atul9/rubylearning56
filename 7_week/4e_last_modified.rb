8026c0588f9b11a2c43bb577b89b6c90ef12a93f
Victor Goff <keeperotphones@gmail.com>
2015-01-02 04:38:49 -0500

    Removed the 'reporting' aspect of the method
    The method extractions in the original implementation leads to me being
    able to pass in a file, and being explicit about that.  But really, it
    will accept anything that is 'file like', it simply needs to respond to
    `mtime` method.
    
    To demonstrate, I require 'pathname' and then use the projects parent
    folder to determine the last modified date.

--- 7_week/4e_last_modified.rb
+++ 7_week/4e_last_modified.rb
@@ -3,9 +3,24 @@ Exercise 4: Write a method last_modified(file) that takes a file nameand
 displays something like this: file was last modified 125.873605469919 days ago.
 Use the Time class.
 =end
-def last_modified(file)
-  seconds_diff = Time.now - File.new(file).mtime
-  days_diff = seconds_diff/ (60 * 60 * 24)
-  "File was last modified #{days_diff} days ago."
+def last_modified(file = File.new(__FILE__, 'r'))
+  difference_in_seconds(file.mtime) / seconds_in_day
 end
-puts last_modified('b1.txt')
+
+# Should this really be a constant?
+def seconds_in_day
+  60 * 60 * 24
+end
+
+def difference_in_seconds(historical_time_value)
+  Time.now - historical_time_value
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "This program was last modified about %.6f days ago" % last_modified
+  puts last_modified(File.new('b1.txt', 'r'))
+
+  require 'pathname'
+  puts "The parent folder of this project was last modified %.2f days ago" %
+    last_modified(Pathname.new('..'))
+end
