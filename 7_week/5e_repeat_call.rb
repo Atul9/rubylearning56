618bbbd6be3a2559abcac7a01d6c6173bc2de8d1
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2015-01-02 16:31:33 +0000

    Call to shell
    Program to call to shell after an interval of 5 seconds. Made use of
    yield and sleep. Proc passed to the method.

--- 7_week/5e_repeat_call.rb
+++ 7_week/5e_repeat_call.rb
@@ -4,15 +4,12 @@ command) repeatedly at a certain interval (for example, once every five seconds
 for one minute). Write a method for this. Do not use Thread class for now.
 Hint: yield and sleep methods may be required.
 =end
-def call_to_shell(block)
+def call_to_shell(&block)
   loop do
     yield block
     sleep 5
   end
 end
-def call_block
-  yield("Command running")
-end
+a = ->(command){ puts "Command running" }
 
-call_block {|str| puts str}
-call_to_shell(call_block)
+call_to_shell(&a)
