ca8e9d55c05c3b66cd27516a3d10bfe73ed71772
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2015-01-03 02:18:17 +0000

    Added behaviour proc
    Passed a proc as an argument to the method and raised an Argument error
    if the argument is not passed.

--- 7_week/5e_repeat_call.rb
+++ 7_week/5e_repeat_call.rb
@@ -4,12 +4,13 @@ command) repeatedly at a certain interval (for example, once every five seconds
 for one minute). Write a method for this. Do not use Thread class for now.
 Hint: yield and sleep methods may be required.
 =end
-def call_to_shell(&block)
-  loop do
-    yield block
-    sleep 5
+def call_to_shell(interval: 5, duration: 5 , behavior: -> {fail ArgumentError, "We need some behaivior:  Provide a lambda or proc?"} )
+  duration.times do
+    behavior.call # We don't need an if, due to the protective default behavior.
+    sleep interval
   end
 end
-a = ->(command){ puts "Command running" }
+a = -> { puts "Command running" }
 
-call_to_shell(&a)
+call_to_shell(interval: 1, duration: 3, behavior: a)
+call_to_shell # This one will raise an informative error
