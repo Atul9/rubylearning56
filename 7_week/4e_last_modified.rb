f72d849595c675c1a00e5bb87e8d995d3d425c39
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2015-01-03 02:40:44 +0000

    Last modification to file.
    Added methods according to the operations they perform. Passed the
    program file as default argument.

--- 7_week/4e_last_modified.rb
+++ 7_week/4e_last_modified.rb
@@ -1,11 +1,22 @@
 =begin
 Exercise 4: Write a method last_modified(file) that takes a file nameand
-displays something like this: file was last modified 125.873605469919 days ago.
+displays something like this: file was last modified 125.873605469919 **days** ago.
 Use the Time class.
 =end
-def last_modified(file)
-  seconds_diff = Time.now - File.new(file).mtime
-  days_diff = seconds_diff/ (60 * 60 * 24)
-  "File was last modified #{days_diff} days ago."
+def last_modified(file_name = __FILE__)
+   modified_age_of(file_name) / seconds_in_day
 end
-puts last_modified('b1.txt')
+
+private
+
+def modified_age_of(filename)
+  Time.now - File.new(filename).mtime
+end
+
+def seconds_in_day
+  60 * 60 * 24
+end
+
+puts last_modified('b1.txt') # This ensures the return is reasonable... just a set of days.
+puts  "File was last modified #{ last_modified 'b1.txt' } days ago." # This gives good context/reporting
+puts last_modified # This one by default checks the current file. 
