3db7d5468b600fa40a9ed57275eb110217276cf3
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2015-01-04 16:37:54 +0000

    Our own fmt in ruby as in Golang

--- pair_programming/our_own_fmt.rb
+++ pair_programming/our_own_fmt.rb
@@ -1,4 +1,6 @@
 # We want a program that does what String#% does
+# I believe this may do just that!  Not as full featured, can you fulfill some
+# of the wishes that are made possible like the original String#% method?
 class String
   def our_fmt(replacement)
     self.gsub(":fmt:", replacement)
@@ -7,3 +9,4 @@ end
 
 puts "This is :fmt: a sample.".our_fmt("now")
 
+puts 'This also :fmt: use single quotes!'.our_fmt('can')
