97eb5ee1f9a1a28fabf2ba6869d8907003d89307
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2015-01-04 16:26:45 +0000

    Done with game cheating program
    Add method load_game, which checks whether the file was changed with the
    help of ctime method. The ctime method return the change time for the
    named file.

--- 6_week/5e_game.rb
+++ 6_week/5e_game.rb
@@ -1,6 +1,10 @@
 =begin
 
-Exercise5. Here's code for the part of a game that saves the game state to a file. As a deterrent against cheating, when the game loads a save file it performs a simple check against the file's modification time. If it differs from the timestamp recorded inside the file, the game refuses to load the save file.
+Exercise5. Here's code for the part of a game that saves the game state to a
+file. As a deterrent against cheating, when the game loads a save file it
+performs a simple check against the file's modification time. If it differs
+from the timestamp recorded inside the file, the game refuses to load the save
+file.
 
 The save_game method is responsible for recording the timestamp:
 
@@ -12,18 +16,52 @@ def save_game(file)
   end
 end
 
-The load_game method is responsible for comparing the timestamp within the file to the time the filesystem has associated with the file. Write the load_game(file) method.
+The load_game method is responsible for comparing the timestamp within the file
+to the time the filesystem has associated with the file. Write the
+load_game(file) method.
 
 This mechanism can detect simple forms of cheating:
 save_game("game.sav")
 sleep(2)
 load_game("game.sav") # => "Your saved score is 1000."
- 
+
 # Now let's cheat by increasing our score to 9000
 open("game.sav", "r+b") { |f| f.write("9") }
 load_game("game.sav") # RuntimeError: I suspect you of cheating.
 
-Since it's possible to modify a file's times with tools like the Unix touch command, you shouldn't depend on these methods to defend you against a skilled attacker actively trying to fool your program. 
+Since it's possible to modify a file's times with tools like the Unix touch
+command, you shouldn't depend on these methods to defend you against a skilled
+attacker actively trying to fool your program. 
 
 Read up on sleep method and Time class.
 =end
+def save_game(file, score =1000)
+  open(file, "w") do |f|
+    f.puts score, Time.new
+  end
+end
+
+def load_game(file)
+  score, time = IO.readlines(file)
+  if cheating?(file, time)
+    raise "This is cheating"
+    print_score(score)
+  end
+end
+
+def cheating?(file, time)
+  time != File.ctime(file)
+end
+
+def print_score(score)
+  "Your saved score is #{score.to_i}"
+end
+
+if __FILE__ == $0
+  save_game("game.sav")
+  sleep(2)
+  # load_game("game.sav")
+  # Now let's cheat by increasing our score to 9000
+  open("game.sav", "r+b") { |f| f.write("9") }
+  load_game("game.sav")
+end
